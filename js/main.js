305074f1372be0862ea3c516fdd1507c4abcbfea
Xenon <SDA152nn@gmail.com>
2024-04-26 16:27:53 +0300

    Блок "Event Technology" Выполнена вёрстка блока Event Technology и добавлена адаптивность. Также немного переработан блок Digital Marketing.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 document.querySelector('.header__mobile-menu-button').addEventListener('click', function(event) {
     let menu = document.querySelector('.header__list');
     menu.classList.toggle('show-menu');
-    document.body.classList.toggle('show-menu');
 })
